11aa0313633ed2540c043990fcb61b6cf85db9d4
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-09 18:07:06 +0100

    testing logs on finished products

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+import logging
 
+logger = logging.getLogger(__name__)
 # Create your views here.
 
 class TodoView(viewsets.ModelViewSet):
